ce35e69ddaf106bf42ed794d2ff64aa69e763dfd
Sergio A. Sánchez Dueñas <99614355+SASD01@users.noreply.github.com>
2022-10-16 23:15:47 -0500

    Delete src directory

--- src/Code/IToken.java
+++
@@ -1,5 +0,0 @@
-package Code;
-public interface IToken {
-    void Add(Character characters); //Add Method. (Type of characterization).
-    void League(Character characters, String League); // League Method. (Group the character belongs to).
-    void Enemy(Character characters);} // Enemy Method. (Add an Enemy for our Character).
